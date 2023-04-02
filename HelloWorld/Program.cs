540ea2eb8b30cff0454859c9c27e01b9f02148a8
beidysy <beydisy@gmail.com>
2023-04-02 05:09:04 -0400

    WP

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        // Entry point of our program
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+//////////
